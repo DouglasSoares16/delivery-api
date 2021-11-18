3d853f9a7dd73d385854d09bd9174c645912d843
Douglas Soares <douglasmaismcpe@gmail.com>
2021-11-18 19:14:29 -0300

    Init Setup

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+import { routes } from "./routes";
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(5500, () => console.log("Server is running ;)"));
