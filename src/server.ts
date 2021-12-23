44a8bb1b1b0ce83a9cdd7d6c272db35597b92f7c
Douglas Soares <douglasmaismcpe@gmail.com>
2021-12-23 19:50:59 -0300

    Using The Error Middleware

--- src/server.ts
+++ src/server.ts
@@ -2,31 +2,14 @@ import "express-async-errors";
 
 import express from "express";
 
+import error from "./middlewares/error";
 import { routes } from "./routes";
-import { AppError } from "./shared/errors/AppError";
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
 
-app.use(
-  (
-    err: Error,
-    request: express.Request,
-    response: express.Response,
-    next: express.NextFunction
-  ) => {
-    if (err instanceof AppError) {
-      return response.status(err.statusCode).json({
-        message: err.message,
-      });
-    }
-
-    return response.status(500).json({
-      message: `Internal Server Error - ${err.message}`,
-    });
-  }
-);
+app.use(error);
 
 app.listen(5500, () => console.log("Server is running ;)"));
