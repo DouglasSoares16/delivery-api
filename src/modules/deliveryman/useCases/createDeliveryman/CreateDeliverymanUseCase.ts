a74acb72455048203d0c8df9fba57f508fbb2ba6
Douglas Soares <douglasmaismcpe@gmail.com>
2021-12-24 10:43:18 -0300

    Created Use Case of Create Deliveryman

---
+++ src/modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanUseCase.ts
@@ -0,0 +1,34 @@
+import { hash } from "bcryptjs";
+
+import { prisma } from "../../../../database/prismaClient";
+import { CreateDeliverymanError } from "./CreateDeliverymanError";
+import { ICreateDeliverymanDTO } from "./ICreateDeliverymanDTO";
+
+class CreateDeliverymanUseCase {
+  async execute({ username, password }: ICreateDeliverymanDTO) {
+    const deliverymanExists = await prisma.deliveryman.findFirst({
+      where: {
+        username: {
+          mode: "insensitive",
+        },
+      },
+    });
+
+    if (deliverymanExists) {
+      throw new CreateDeliverymanError.DeliverymanAlreadyExists();
+    }
+
+    const passwordHash = await hash(password, 10);
+
+    const deliveryman = await prisma.deliveryman.create({
+      data: {
+        username,
+        password: passwordHash,
+      },
+    });
+
+    return deliveryman;
+  }
+}
+
+export { CreateDeliverymanUseCase };
